b2c3784f37c94165dccbc219f523930f8fda211c
Perseverance <nsiloup@gmail.com>
2021-08-15 10:15:16 +0100

    code

--- src/Components/MarkdownPrev.jsx
+++ src/Components/MarkdownPrev.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 //import ReactMarkdown from 'react-markdown';
 //import  gfm  from 'remark-gfm';
 import marked from 'marked';
+import Prism from "prismjs";
+import DOMPurify from 'dompurify';
+
 
 class MarkdownPrev extends React.Component {
     constructor(props){
@@ -10,24 +13,41 @@ class MarkdownPrev extends React.Component {
             input : ``
         };
         this.handleChange = this.handleChange.bind(this);
-        this.getMrkDwn = this.getMrkDwn.bind(this);
+        //this.getMrkDwn = this.getMrkDwn.bind(this);
     };
     handleChange(event){
         this.setState({
             input : event.target.value
         })
     }
-    getMrkDwn = (mrkInput) =>{
-        return { __html : marked(mrkInput, /*option */)}
-    };
     render(){
         let { input } = this.state;
+
+        let getMrkDwn = (mrkInput) =>{
+            let markdFunc = marked(mrkInput,
+                [
+                    marked.setOptions({
+
+                        breaks: true,
+                        tables : true,
+                        highlight: function (code) {
+                            return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+                        }                        
+                    }),
+                    
+                ]
+                /*option */)
+                let cleaned = DOMPurify.sanitize(markdFunc);
+                console.log(cleaned)
+            return { __html : /*markdFunc*/cleaned}
+        };
+            
         //console.log(ReactMarkdown)
         return(
             <div>
-                <textarea id="editor" onChange={this.handleChange} value={input} />
+                <textarea id="editor" onChange={this.handleChange} value={input} className="edit"/>
                 {/*<ReactMarkdown remarkPlugins={[gfm]} children={input} id="preview"/>*/}
-                <div dangerouslySetInnerHTML = {this.getMrkDwn(input)} />
+                <div dangerouslySetInnerHTML = {getMrkDwn(input)} id="preview" className="prev"/>
             </div>
         )
     };
